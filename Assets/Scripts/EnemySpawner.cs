65c212b5b1c126ea3fb475dbe94d4edb819ca607
ethan <ecgetman18@earlham.edu>
2020-11-02 17:02:56 -0600

    11/2/2020
    added an enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemy;
+    float randX;
+    Vector2 whereToSpawn;
+    private float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = UnityEngine.Random.Range(-6f, 6f);
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(enemy, whereToSpawn, Quaternion.identity);
+        }
+    }
+}
